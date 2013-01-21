b58c6e3ac98ddb688f14374d2a802c0614a734b5
bamanuel <bamanuel@gmail.com>
2013-01-20 22:46:13 -0600

    almost working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,16 +7,28 @@ import play.api.Play.current
 import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.json
-import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee._
 import models._
+import play.api.libs.Comet
+
+import akka.dispatch.Await
+import akka.dispatch.Future
+import java.util.concurrent.TimeUnit
+import akka.util._
+import akka.util.duration._
+
 
 object Application extends Controller {
   
 	def index = Action {
-		val data = Dining.runDinings
-		val dataContent:Enumerator[Array[Byte]] = Enumerator.fromCallback(data)
+		/*val data = Dining.runDinings
+		val dataContent:Enumerator[String] = Enumerator.fromCallback(data)
 		//for(i <- dataContent){println(i)}
-		Ok.stream(dataContent.andThen(Enumerator.eof))
+		//Iteratee.foreach[Array[Byte]](msg => println(new String(msg)))
+
+		//Ok.stream(dataContent.andThen(Enumerator.eof) &> Comet(callback = "console.log"))
+		Ok.stream(dataContent.andThen(Enumerator.eof))*/
+		Ok("test")
 	}
 	
 	def start = Action {
@@ -28,10 +40,14 @@ object Application extends Controller {
 		Dining.stopDining
 		Ok("End")
 	}
-	
+	val data = Dining.runDinings
 	def act = Action { implicit request =>
+		val timeout = Timeout(10 seconds)
+		val result = Await.result(data().mapTo[List[String]], timeout.duration)//.asInstanceOf[Strings]
+		println(result) 
+
 		// to get a Blah object from request content
-		val blah = json.Json.parse(request.body.asText.get).as[String]
+		val blah = json.Json.toJson(result).as[List[String]]
 	
 		// to return Blah as application/json, you just have to convert your Blah to a JsValue and give it to Ok()
 		Ok(json.Json.toJson(blah))
--- app/controllers/Philosophers.scala
+++ app/controllers/Philosophers.scala
@@ -8,8 +8,10 @@ import akka.util.duration._
 import java.util.concurrent.TimeUnit
 import akka.util._
 //import scala.concurrent.duration._
+import akka.dispatch.{Await, Future, ExecutionContext} 
 import akka.pattern._
 import play.api.libs.concurrent._
+import java.util.concurrent.Executors
 
 object Take
 object Taken
@@ -108,15 +110,14 @@ class Restaurant extends Actor {
 
 	def mainStream:Receive = {
 		case Dump =>
-			val data:Array[Byte] = messages.flatMap(msg => msg.toCharArray.map(_.toByte)).toArray//messages.foreach(msg => msg.toCharArray.map(_.toByte))
-			
+			val data:List[String] = messages.reverse
 			//println(new String(data))
 			sender ! data
 			messages = List()//empty list
 			//actorStream |>> Iteratee.forEach[String](s => println(s))
 		case msg:String =>
-			//println(msg)
-			messages = msg :: messages
+			println(msg)
+			messages = msg+"\r\n" :: messages
 	}
 	def toHexString(bytes:Array[Byte]) : String = {
       val sb = new StringBuilder();
@@ -139,12 +140,28 @@ object Dining {
 		val philosophers = for((name, i) <- List("A","B","C","D","E").zipWithIndex) yield system.actorOf(Props(new Philosopher(name,chopsticks(i), chopsticks((i+1) % 5),restaurant)),name) 
 		philosophers.foreach(_ ! Eat)
 		restaurant ! "Dinner is served."
-		//restaurantObj.actorStream
+		
+		/*implicit val timeout = Timeout(10 seconds)
+		val future = ask(restaurant, Dump)
+
+		val result = Await.result(future.mapTo[Option[String]], timeout.duration)//.asInstanceOf[Strings]
+		println(result)*/
 
 		val actorStream = { () =>
+
+			val pool = Executors.newCachedThreadPool() 
+    implicit val ec = ExecutionContext.fromExecutorService(pool) 
+			val test = Future {
+			  List(" future!", "abd", "123")
+			}
 			//Promise.timeout(Some(restarant ? Dump), 100 milliseconds)
-			(restaurant ? Dump)(5 seconds).mapTo[Option[Array[Byte]]].asPromise//.asPromise
+			//println("received request")
+			implicit val timeout = Timeout(20 seconds)
+			val future = (restaurant ? Dump)
+			//println(Await.result(future, timeout.duration))
+			future.mapTo[List[String]]//.asPromise
 			//return new Promise(new String(), 100);
+			test.mapTo[List[String]]
 		}
 		actorStream
 	}
